ebb30492e4a29038c9556700321daff6ba46e1a1
FlemingJohn <flemjohn08@gmail.com>
2026-09-18 12:13:41 +0530

    feat: add the defend meta type

---
+++ src/defend/types/DefendMeta.ts
@@ -0,0 +1,8 @@
+export interface DefendMeta {
+  source: string;
+  builtAt: string;
+  countermeasureCount: number;
+  mappedTechniqueCount: number;
+  pairCount: number;
+  uncoveredDomains: string[];
+}
